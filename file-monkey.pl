85340c089f7b401fe82cfb48627a5e1054522f3d
Michael Conrad <mconrad@intellitree.com>
2018-09-05 14:33:35 -0400

    Fixed numerous bugs and added an option to run in parallel
      * Added --monkeys=N option.
      * Added sanity check for symlinks.
      * Ignore any directories shorter than length created by this script.
      * Don't depend on usr or sbin symlinks.
      * Resync directory cache more often, to deal with parallel workers.

--- file-monkey.pl
+++ file-monkey.pl
@@ -9,9 +9,17 @@ file-monkey.pl - create filesystem activity simulating a samba fileserver worklo
 file-monkey.pl [options] # no options required
 
  Option Summary:
-   --help               brief help
-   --man                full documentation
-                     
+   --root ROOT          root directory for files being randomized
+   --chroot             chroot into ROOT.  requires /bin/busybox.static
+   --no-chroot          do not chroot into ROOT (but still chdir)
+   --max-dirs N         maximum number of directories allowed in ROOT
+   --max-disk-usage N   max bytes of files allowed in ROOT
+   --monkeys N          run multiple copies in parallel on the same ROOT
+                        (note that size restrictions might not be fully enforced)
+   
+   The default is to chroot to /opt/site/file-monkey and work on 10GB of files,
+   max 8000 directories, with a single process making changes every 1-8 seconds.
+   
    --mkdir X            probability of creating new directory
    --rmdir X            probability of recursively deleting a directory
    --mkfile X           probability of creating a file
@@ -20,15 +28,6 @@ file-monkey.pl [options] # no options required
    
    All probabilities are numbers (0..1), and must add up to less than 1.  Unused
    probability range will be the probability of re-writing a file.
-   
-   --root ROOT          root directory for file activity.  will be created if missing.
-   --chroot             chroot into ROOT.  requires /bin/busybox.static
-   --no-chroot          do not chroot into ROOT (but still chdir)
-   --max-dirs N         maximum number of directories allowed in ROOT
-   --max-disk-usage N   max bytes of files allowed in ROOT
-   
-   The default is to chroot to /opt/site/file-monkey and work on 10GB of files,
-   max 8000 directories.
 
 =cut
 
@@ -52,9 +51,8 @@ my $dir_leaf_limit= 8000;
 my $size_limit= 10*1024*1024*1024;
 my $chroot= 1;
 
+sub pod2usage { require Pod::Usage; goto &Pod::Usage::pod2usage; }
 GetOptions (
-	'help|?'           => sub { pod2usage(1); },
-	'man'              => sub { pod2usage(-exitval => 1, -verbose => 2); },
 	'mkdir=f'          => \$mkdir_chance,
 	'rmdir=f'          => \$rmdir_chance,
 	'mkfile=f'         => \$mkfile_chance,
@@ -64,32 +62,64 @@ GetOptions (
 	'max-dirs=i'       => \$dir_leaf_limit,
 	'max-disk-usage=i' => \$size_limit,
 	'chroot!'          => \$chroot,
+	'monkeys=i'        => \(my $monkeys= 1),
+	'help|?'           => sub { pod2usage(1); },
+	'man'              => sub { pod2usage(-exitval => 1, -verbose => 2); },
 ) or pod2usage();
 
 die "Invalid root \"$root\"\n"
 	unless defined $root and length $root and index($root,"'") == -1;
 
-`mkdir -p '$root'`;
-$? == 0 or die "mkdir failed";
+system('mkdir', -p => $root) == 0 and -d $root
+	or die "mkdir failed: $?";
 $root= realpath($root);
 die "Cowardly refusing to run in \"$root\" because it isn't 3 directories deep\n"
 	unless 2 < scalar(()= $root =~ m|(/)|g);
 chdir $root or die "Can't chdir to $root: $!";
 
 if ($chroot) {
-	`mkdir -p ./bin`;
-	`cp /bin/busybox.static ./bin/`;
+	umask 0007;
+	-x '/bin/busybox.static' or die "Require '/bin/busybox.static' unless --no-chroot given";
+	mkdir("./bin");
+	system('cp','/bin/busybox.static','./bin/') == 0 or die "cp /bin/busybox.static ./bin/ failed";
 	link('./bin/busybox.static', './bin/find');
 	link('./bin/busybox.static', './bin/rm');
-	symlink('bin', './sbin');
-	symlink('.', './usr');
-	chroot '.' or warn "Can't chroot: $!";
+	chroot '.' or die "Can't chroot: $!  Use --no-chroot";
 }
 
 my $str_rand= String::Random->new;
 my @subpaths;
 my $disk_usage;
+my $dirname_length= 8; # must always be longer than "bin" and ".", else they might get deleted or modified
+my $delay_max= 5;
 my $file_count;
+my $parent_pid;
+sub run;
+
+# Optionally run lots of monkeys in parallel, on the same directories.
+if ($monkeys > 1) {
+	$parent_pid= $$;
+	my $n= 0;
+	while (1) { # run until killed
+		if ($n < $monkeys) {
+			my $child= fork // die "fork: $!";
+			if ($child) {
+				print STDERR "Started monkey $child\n";
+				++$n;
+				next;
+			} else {
+				srand(time % $$);
+				run;
+				exit 0;
+			}
+		}
+		--$n if wait > 0;
+		sleep 1; # just in case
+	}
+}
+else {
+	run;
+}
 
 sub refresh_tree_stats {
 	@subpaths= ();
@@ -100,65 +130,80 @@ sub refresh_tree_stats {
 		chomp;
 		lstat;
 		if (-d _) {
-			push @subpaths, $_ unless $_ eq '.' or $_ =~ /^\.\/bin/;
+			push @subpaths, $_ if length($_) == $dirname_length;
 		}
 		elsif (-f _) {
 			$file_count++;
 			$disk_usage+= -s $_;
 		}
+		elsif (-l _) {
+			warn "Symlinks found in tree! ($_) Aborting";
+			exit 2;
+		}
 	}
 	print STDERR "(".@subpaths." dirs, $disk_usage bytes in $file_count files)\n";
 }
 
-refresh_tree_stats;
-while (1) {
-	# Wait 1 to 8 seconds
-	sleep int(rand 8);
-	try {
-		my $choice= rand;
-		my $dir= pick_random_dir();
-		my $fname= pick_random_file();
-		
-		# Create a new directory?
-		if (($choice-= $mkdir_chance) < 0 or !defined $dir) {
-			print "new dir,  ";
-			print create_dir()."\n";
+sub run {
+	refresh_tree_stats;
+	while (1) {
+		# Wait 1 to 8 seconds
+		sleep int(rand $delay_max);
+		# If running as child worker, check parent is still alive
+		if ($parent_pid) {
+			kill 0, $parent_pid or exit 0;
 		}
-		# Remove an entire directory?
-		elsif (($choice-= $rmdir_chance) < 0 or @subpaths > $dir_leaf_limit) {
-			print "rmdir $dir,  ";
-			print rm_dir($dir)."\n";
+		try {
+			my $choice= rand;
+			my $dir= pick_random_dir();
+			my $fname= pick_random_file();
+			
+			# Create a new directory?
+			if (($choice-= $mkdir_chance) < 0 or !defined $dir) {
+				print "new dir,  ";
+				print create_dir()."\n";
+			}
+			# Remove an entire directory?
+			elsif (($choice-= $rmdir_chance) < 0 or @subpaths > $dir_leaf_limit) {
+				print "rmdir $dir,  ";
+				print rm_dir($dir)."\n";
+			}
+			# Create a new file?
+			elsif (($choice-= $mkfile_chance) < 0 or !defined $fname) {
+				print "new file in $dir,  ";
+				print create_file($dir)."\n";
+			}
+			# Remove a file?
+			elsif (($choice-= $rmfile_chance) < 0 or $disk_usage > $size_limit) {
+				print "rm file $fname,  ";
+				print rm_file($fname)."\n";
+			}
+			# Append a file?
+			elsif (($choice-= $append_chance) < 0) {
+				print "append file $fname,  ";
+				print append_file($fname)."\n";
+			}
+			# else rewrite file
+			else {
+				print "rewrite file $fname,  ";
+				print rewrite_file($fname)."\n";
+			}
 		}
-		# Create a new file?
-		elsif (($choice-= $mkfile_chance) < 0 or !defined $fname) {
-			print "new file in $dir,  ";
-			print create_file($dir)."\n";
+		catch {
+			chomp($_);
+			warn "\n$_";
+			refresh_tree_stats();
 		}
-		# Remove a file?
-		elsif (($choice-= $rmfile_chance) < 0 or $disk_usage > $size_limit) {
-			print "rm file $fname,  ";
-			print rm_file($fname)."\n";
-		}
-		# Append a file?
-		elsif (($choice-= $append_chance) < 0) {
-			print "append file $fname,  ";
-			print append_file($fname)."\n";
-		}
-		# else rewrite file
-		else {
-			print "rewrite file $fname,  ";
-			print rewrite_file($fname)."\n";
-		}
-	}
-	catch {
-		chomp($_);
-		warn "\n$_";
-		refresh_tree_stats();
 	}
 }
 
 sub pick_random_dir {
-	@subpaths? $subpaths[ int rand @subpaths ] : undef;
+	my $p= @subpaths? $subpaths[ int rand(scalar @subpaths) ] : undef;
+	if ($p && !-d $p) { # if some other monkey deleted the path, refresh the list
+		refresh_tree_stats();
+		$p= @subpaths? $subpaths[ int rand(scalar @subpaths) ] : undef;
+	}
+	return $p;
 }
 
 sub pick_random_file {
@@ -166,8 +211,8 @@ sub pick_random_file {
 	return undef unless @subpaths;
 	for (my $i= 0; $i < 15; $i++) {
 		my $d= pick_random_dir();
-		my @files= grep { -f $_ } <$d/*>;
-		return $files[ int rand @files ]
+		my @files= grep { lstat && -f _ && $_ !~ /~$/ } <$d/*>;
+		return $files[ int rand(scalar @files) ]
 			if @files;
 	}
 	return undef;
@@ -175,7 +220,7 @@ sub pick_random_file {
 
 sub create_file {
 	my $dir= shift;
-	my $fname= $dir . '/' . $str_rand->randregex('\w\w\w\w\w\w\w\w\w\w');
+	my $fname= $dir . '/' . $str_rand->randregex('\w' x $dirname_length);
 	# don't bother checking to be unique, doesn't really matter if it fails
 	open my $fh, ">", $fname
 		or die "open($fname): $!";
@@ -190,7 +235,7 @@ sub create_file {
 sub append_file {
 	my $fname= shift;
 	my $s1= -s $fname;
-	open my $fh, ">>$fname" or die "$!";
+	open my $fh, '>>', $fname or die "$!";
 	print $fh $str_rand->randpattern("." x 1024)
 		for 0 .. int rand 1024;
 	close $fh or warn "close($fname): $!";
@@ -201,12 +246,12 @@ sub append_file {
 sub rewrite_file {
 	my $fname= shift;
 	my $s1= -s $fname;
-	open my $fh, ">.new-$fname" or die "$!";
+	open my $fh, '>', "$fname~" or die "open($fname~): $!";
 	print $fh $str_rand->randpattern("." x 1024)
 		for 0 .. $s1/1024;
 	close $fh;
 	unlink($fname) or warn "unlink($fname): $!";
-	rename(".new-$fname", $fname) or warn "rename($fname): $!";
+	rename("$fname~", $fname) or warn "rename($fname~): $!";
 	$disk_usage+= -$s1 + -s $fname;
 	1;
 }
@@ -221,7 +266,7 @@ sub rm_file {
 }
 
 sub create_dir {
-	my $i= int rand 1+@subpaths;
+	my $i= int rand(1+@subpaths);
 	my $dir= ($i >= @subpaths)? '.' : $subpaths[$i];
 	my $path= $dir . '/' . $str_rand->randregex('\w\w\w\w\w\w\w\w');
 	# don't bother checking to be unique, doesn't really matter if it fails
